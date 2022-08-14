d6e6b2061b0121a02aab2b35a1c93b65076de448
phs <gustjd0808@pusan.ac.kr>
2022-08-14 13:26:56 +0900

    프로젝트 설정

---
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,46 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Commit // 테스트하면서 DB에 데이터 보기 위해서
+class QuerydslApplicationTests {
+
+	@PersistenceContext // or @Autowired
+	EntityManager em;
+
+	@Test
+	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHello qHello = new QHello("h"); // 대신 사용 가능 -> QHello qHello = QHello.hello;
+
+
+		/**
+		 * queryDsl을 쓸 땐 쿼리와 관련된 건 모두 Q타입을 써야한다.
+		 */
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+
+		assertThat(result).isEqualTo(hello);
+		assertThat(result.getId()).isEqualTo(hello.getId());
+
+	}
+
+}
