c8d2c0e8035afe4b2c667d420425d9be74151a8b
phs <gustjd0808@pusan.ac.kr>
2022-08-16 12:28:25 +0900

    프로젝션과 결과 반환 - 기본

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -556,4 +556,44 @@ public class QuerydslBasicTest {
         }
     }
 
+
+    /**
+     * [프로젝션과 결과 반환 - 기본]
+     * 프로젝션: select 대상 지정
+     *
+     * 1. 프로젝션 대상이 하나
+     * - 프로젝션 대상이 하나면 타입을 명확하게 지정할 수 있음
+     *
+     * 2. 튜플 조회
+     * - 프로젝션 대상이 둘 이상일 때 사용
+     * - 프로젝션 대상이 둘 이상이면 튜플이나 DTO로 조회
+     *
+     * 참고: 튜플 타입의 경우 queryDsl에 종속적이니 리포지토리 안에서만 쓰는 것을 권장하고
+     *      애플리케이션 단 등 외부로 데이터를 내보낼 땐 DTO를 사용하자.
+     */
+    @Test
+    public void simpleProjection(){
+        List<String> result = queryFactory
+                .select(member.username)
+                .from(member)
+                .fetch();
+
+        for(String username:result){
+            System.out.println("username = " + username);
+        }
+    }
+
+    @Test
+    public void tupleProjection(){
+        List<Tuple> result = queryFactory
+                .select(member.username, member.age)
+                .from(member)
+                .fetch();
+
+        for(Tuple tuple: result){
+            System.out.println("username = " + tuple.get(member.username));
+            System.out.println("age = " + tuple.get(member.age));
+        }
+    }
+
 }
