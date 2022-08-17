03a37c22f2cf793e1c1c3f1d41c942fa720bd2ce
phs <gustjd0808@pusan.ac.kr>
2022-08-18 00:28:52 +0900

    스프링 데이터 JPA 리포지토리로 변경 + 사용자 정의 리포지토리

---
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -0,0 +1,72 @@
+package study.querydsl.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+    @Autowired
+    EntityManager em;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void basicTest(){
+        Member member = new Member("member1", 10);
+        memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(member.getId()).get(); // 바로 get하는 건 좋지 않으나 예제라 이렇게 함
+        assertThat(findMember).isEqualTo(member);
+
+        List<Member> result1 = memberRepository.findAll();
+        assertThat(result1).containsExactly(member);
+
+        List<Member> result2 = memberRepository.findByUsername(member.getUsername());
+        assertThat(result2).containsExactly(member);
+    }
+
+    @Test
+    public void searchTest(){
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCondition memberSearchCondition = new MemberSearchCondition();
+
+        // 조건이 아무것도 없을 경우 모든 데이터를 끌어 오게 되면서 성능 이슈가 발생할 수 있다.
+        // 따라서 기본 조건을 걸거나 페이징을 추가하자.
+        memberSearchCondition.setAgeGoe(35);
+        memberSearchCondition.setAgeLoe(40);
+        memberSearchCondition.setTeamName("teamB");
+
+        //List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(memberSearchCondition);
+
+        // 코드 재사용성과 가독성 때문에 where 절 파라미터를 이용한 동적쿼리가 많이 쓰이지만 Builder가 쓰일 때도 있다.
+        List<MemberTeamDto> result = memberRepository.search(memberSearchCondition);
+
+        assertThat(result).extracting("username").containsExactly("member4");
+    }
+}
