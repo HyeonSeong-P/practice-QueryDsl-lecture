9e8268aa0da903ece3948b73cf62e68537477964
phs <gustjd0808@pusan.ac.kr>
2022-08-18 10:41:18 +0900

    스프링 데이터 페이징 활용 - CountQuery 최적화 + 컨트롤러 개발

--- src/main/java/study/querydsl/repository/MemberRepositoryCustomImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryCustomImpl.java
@@ -1,14 +1,17 @@
 package study.querydsl.repository;
 
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.util.StringUtils;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
+import study.querydsl.entity.Member;
 
 import javax.persistence.EntityManager;
 import java.util.List;
@@ -51,15 +54,30 @@ public class MemberRepositoryCustomImpl implements MemberRepositoryCustom{
     public Page<MemberTeamDto> searchPage(MemberSearchCondition condition, Pageable pageable) {
         List<MemberTeamDto> content = getMemberTeamDtos(condition, pageable);
 
-        long total = getTotal(condition);
+        //long total = getTotal(condition);
+        JPAQuery<Member> countQuery = queryFactory
+                .select(member)
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                );
 
-        return new PageImpl<>(content, pageable, total);
+        /** [PageableExecutionUtils.getPage을 이용한 count query 최적화]
+         *  count 쿼리가 생략 가능한 경우 알아서 생략해서 처리한다.
+         *      - 페이지 시작이면서 컨텐츠 사이즈가 페이지 사이즈보다 작을 때
+         *      - 마지막 페이지일 때 (offset + 컨텐츠 사이즈를 더해서 전체 사이즈 구함)
+         */
+        return PageableExecutionUtils.getPage(content, pageable, () -> countQuery.fetch().size());
 
     }
 
     private long getTotal(MemberSearchCondition condition) {
         long total = queryFactory
-                .select(member.count())
+                .select(member)
                 .from(member)
                 .leftJoin(member.team, team)
                 .where(
